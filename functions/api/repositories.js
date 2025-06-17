3d61acd86b52efe42703d025b6aa4d4893724b37
soepad <soepad@chaoshi.serv00.net>
2025-06-17 22:20:49 +0800

    Update repositories.js

--- functions/api/repositories.js
+++ functions/api/repositories.js
@@ -299,7 +299,7 @@ export async function onRequest(context) {
   }
   
   // 创建新仓库
-  if (path === '/create' && request.method === 'POST') {
+  if (path === 'create' && request.method === 'POST') {
     console.log('匹配到创建仓库路径');
     try {
       console.log('收到创建仓库请求');
